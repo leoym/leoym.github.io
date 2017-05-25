1fd415f2299cb00f378469ee27767b364f408604
Leonardo <leoyasu@yahoo.com.br>
2017-05-25 18:15:09 -0300

    ttt

---
+++ sw.js
@@ -0,0 +1,29 @@
+mportScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/shop/',
+       '/shop/index.html',
+       '/shop/loja-adidas.html',
+       '/shop/loja-bacio-di-latte.html',
+       '/shop/loja-brooksfield.html'
+       '/shop/loja-burberry.html'
+
+     ]);
+   })
+ );
+});
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
