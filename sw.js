41f42d58e59b2da5906369fa91ea4290a30c4b00
Leonardo <leoyasu@yahoo.com.br>
2017-05-25 18:27:31 -0300

    ttt

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@ self.addEventListener('install', function(e) {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
