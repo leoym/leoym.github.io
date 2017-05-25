c7f0efcfbefb402455416bf30d13de18e701153f
Leonardo <leoyasu@yahoo.com.br>
2017-05-25 18:23:06 -0300

    ttt

--- sw.js
+++ sw.js
@@ -4,12 +4,12 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('airhorner').then(function(cache) {
      return cache.addAll([
-       '/shop/',
-       '/shop/index.html',
-       '/shop/loja-adidas.html',
-       '/shop/loja-bacio-di-latte.html',
-       '/shop/loja-brooksfield.html'
-       '/shop/loja-burberry.html'
+       '/',
+       '/index.html',
+       '/loja-adidas.html',
+       '/loja-bacio-di-latte.html',
+       '/loja-brooksfield.html'
+       '/loja-burberry.html'
 
      ]);
    })
